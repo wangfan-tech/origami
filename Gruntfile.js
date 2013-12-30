1742fb72c4c825f41d87b94052367ade435cb898
yubin <gghard@163.com>
2013-12-30 16:22:56 +0800

    内部整合完毕

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function (grunt) {
                 port: 9000,
                 livereload: 35729,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: '0.0.0.0'
+                hostname: 'localhost'
             },
             livereload: {
                 options: {
